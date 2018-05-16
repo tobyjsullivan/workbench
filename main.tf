ef9fb3b2bbcd78af23c5fba5582df1c85f20e239
Toby Sullivan <tobyjsullivan@gmail.com>
2018-05-16 23:29:44 +1000

    Clean up formatting

--- main.tf
+++ main.tf
@@ -15,26 +15,25 @@ provider "aws" {
 resource "aws_security_group" "ssh_in" {
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
   }
 }
 
 resource "aws_key_pair" "workbench" {
   key_name_prefix = "workbench"
-  public_key = "${var.public_key}"
+  public_key      = "${var.public_key}"
 }
 
 resource "aws_instance" "workbench" {
-  ami = "ami-d38a4ab1"
+  ami           = "ami-d38a4ab1"
   instance_type = "t2.micro"
 
   security_groups = ["${aws_security_group.ssh_in.name}"]
-  key_name = "${aws_key_pair.workbench.key_name}"
+  key_name        = "${aws_key_pair.workbench.key_name}"
 }
 
 output "ip_address" {
   value = "${aws_instance.workbench.public_ip}"
 }
-
